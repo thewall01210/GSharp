f8df00c0d5192c543ad17fe007093a91edd0f42e
Justin Ryder <justin@justin-ryder.com>
2014-06-25 21:52:46 -0400

    Added sandbox project with hello world RenderForm window.

---
+++ src/GSharp.Graphics.SDX.Sandbox/Program.cs
@@ -0,0 +1,14 @@
+﻿using SlimDX.Windows;
+
+namespace GSharp.Graphics.SDX.Sandbox
+{
+  static class Program
+  {
+    static void Main()
+    {
+      // http://slimdx.org/tutorials/BasicWindow.php
+      var form = new RenderForm("Sandbox");
+      MessagePump.Run(form, () => { });
+    }
+  }
+}
