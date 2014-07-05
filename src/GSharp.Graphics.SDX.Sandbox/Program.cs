545bc0a009a251751865b2a9971fd22b480db85e
Justin Ryder <justin@justin-ryder.com>
2014-07-04 23:01:27 -0400

    Implemented tutorial 2

--- src/GSharp.Graphics.SDX.Sandbox/Program.cs
+++ src/GSharp.Graphics.SDX.Sandbox/Program.cs
@@ -1,4 +1,10 @@
-﻿using SlimDX.Windows;
+﻿using System.Windows.Forms;
+using SlimDX;
+using SlimDX.Direct3D11;
+using SlimDX.DXGI;
+using SlimDX.Windows;
+using Device = SlimDX.Direct3D11.Device;
+using Resource = SlimDX.Direct3D11.Resource;
 
 namespace GSharp.Graphics.SDX.Sandbox
 {
@@ -6,9 +12,57 @@ namespace GSharp.Graphics.SDX.Sandbox
   {
     static void Main()
     {
-      // http://slimdx.org/tutorials/BasicWindow.php
       var form = new RenderForm("Sandbox");
-      MessagePump.Run(form, () => { });
+      var description = new SwapChainDescription()
+      {
+        BufferCount = 1,
+        Usage = Usage.RenderTargetOutput,
+        OutputHandle = form.Handle,
+        IsWindowed = true,
+        ModeDescription = new ModeDescription(0, 0, new Rational(60, 1), Format.R8G8B8A8_UNorm),
+        SampleDescription = new SampleDescription(1, 0),
+        Flags = SwapChainFlags.AllowModeSwitch,
+        SwapEffect = SwapEffect.Discard
+      };
+
+      Device device;
+      SwapChain swapChain;
+      Device.CreateWithSwapChain(DriverType.Hardware, DeviceCreationFlags.None, description, out device, out swapChain);
+
+      // create a view of our render target, which is the backbuffer of the swap chain we just created
+      RenderTargetView renderTarget;
+      using (var resource = Resource.FromSwapChain<Texture2D>(swapChain, 0))
+        renderTarget = new RenderTargetView(device, resource);
+
+      // setting a viewport is required if you want to actually see anything
+      var context = device.ImmediateContext;
+      var viewport = new Viewport(0.0f, 0.0f, form.ClientSize.Width, form.ClientSize.Height);
+      context.OutputMerger.SetTargets(renderTarget);
+      context.Rasterizer.SetViewports(viewport);
+
+      // prevent DXGI handling of alt+enter, which doesn't work properly with Winforms
+      using (var factory = swapChain.GetParent<Factory>())
+        factory.SetWindowAssociation(form.Handle, WindowAssociationFlags.IgnoreAltEnter);
+
+      // handle alt+enter ourselves
+      form.KeyDown += (o, e) =>
+      {
+        if (e.Alt && e.KeyCode == Keys.Enter)
+          swapChain.IsFullScreen = !swapChain.IsFullScreen;
+      };
+
+      MessagePump.Run(form, () =>
+      {
+        // clear the render target to a soothing blue
+        context.ClearRenderTargetView(renderTarget, new Color4(0.5f, 0.5f, 1.0f));
+        swapChain.Present(0, PresentFlags.None);
+      });
+
+      // clean up all resources
+      // anything we missed will show up in the debug output
+      renderTarget.Dispose();
+      swapChain.Dispose();
+      device.Dispose();
     }
   }
 }
