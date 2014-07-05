41d3caa8c353ae41ea22008de7dc5d7dd3b5bd01
Justin Ryder <justin@justin-ryder.com>
2014-07-04 23:30:31 -0400

    Integrated StructureMap. Added IMainProgram and sandbox implementaiton.

---
+++ src/GSharp.Graphics.SDX.Sandbox/SdxSandboxRegistry.cs
@@ -0,0 +1,12 @@
+﻿using StructureMap.Configuration.DSL;
+
+namespace GSharp.Graphics.SDX.Sandbox
+{
+  public sealed class SdxSandboxRegistry : Registry
+  {
+    public SdxSandboxRegistry()
+    {
+      For<IMainProgram>().Use<SdxSandboxMainProgram>();
+    }
+  }
+}
