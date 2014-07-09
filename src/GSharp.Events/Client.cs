2ff8324d6dcd8f170b13d564559ec8c24205dc05
Alex Toulan <aj.toulan@gmail.com>
2014-07-09 12:02:02 -0400

    Added project for Gsharp.Events\nThis will give us local and network based event capability. I also added a test project inorder to test the publish subscribe cabability. Right now they are called Server and Client. Thiswill be changed to reflect their desired funcitonality.

---
+++ src/GSharp.Events/Client.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using NetMQ;
+using NetMQ.zmq;
+
+namespace GSharp.Events
+{
+  public class Client
+  {
+    private readonly NetMQContext _context;
+
+    private readonly string _id;
+
+    public Client(NetMQContext context, string id)
+    {
+      _context = context;
+      _id = id;
+    }
+
+    public Task Start(CancellationToken token, string address)
+    {
+      var ready = new TaskCompletionSource<bool>();
+
+      Task.Factory.StartNew(
+        () => StartNewConnection(token, address, ready),
+        token, TaskCreationOptions.LongRunning,
+        TaskScheduler.Default);
+
+      return ready.Task;
+    }
+
+    private void StartNewConnection(CancellationToken token, string address, TaskCompletionSource<bool> ready)
+    {
+      using (var socket = _context.CreateRequestSocket())
+      {
+        socket.Connect(address);
+        ready.SetResult(true);
+
+        while (!token.IsCancellationRequested)
+        {
+          var instantMessage = Console.ReadLine();
+          if (!string.IsNullOrEmpty(instantMessage))
+          {
+            socket.Send(Encoding.ASCII.GetBytes(string.Format("Server: {0}", instantMessage)));
+          }
+        }
+      }
+    }
+  }
+}
